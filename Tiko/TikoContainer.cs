efe54920af654b0a631d86b5126c502aa18d4a42
Kyle Lacy <kylewlacy@me.com>
2013-09-28 09:42:34 -0700

    Add `Resolves` attribute
    Automatically calls `TikoContainer.Resolve` at runtime

--- Tiko/TikoContainer.cs
+++ Tiko/TikoContainer.cs
@@ -5,151 +5,189 @@ using System.Reflection;
 
 namespace Tiko
 {
-    public static class TikoContainer
-    {
-        private static List<RegisteredObject> _registeredObjects =
-            new List<RegisteredObject>();
+	public static class TikoContainer
+	{
+		private static List<RegisteredObject> _registeredObjects =
+			new List<RegisteredObject> ();
 
-        /// <summary>
-        /// Resolve dependencies on existing object.
-        /// </summary>
-        /// <typeparam name="T">Type of object.</typeparam>
-        /// <param name="existing">Instance of the object.</param>
-        /// <exception cref="DependencyMissingException">Throw exception if unable to resove a dependency.</exception>
-        /// <returns>Result object.</returns>
-        public static T BuildUp<T>(T existing)
-        {
-            return DoBuildUp(existing);
-        }
+		/// <summary>
+		/// Resolve dependencies on existing object.
+		/// </summary>
+		/// <typeparam name="T">Type of object.</typeparam>
+		/// <param name="existing">Instance of the object.</param>
+		/// <exception cref="DependencyMissingException">Throw exception if unable to resove a dependency.</exception>
+		/// <returns>Result object.</returns>
+		public static T BuildUp<T> (T existing)
+		{
+			return DoBuildUp (existing);
+		}
 
-        /// <summary>
-        /// Clear the container.
-        /// </summary>
-        public static void Clear()
-        {
-            _registeredObjects = new List<RegisteredObject>();
-        }
+		/// <summary>
+		/// Clear the container.
+		/// </summary>
+		public static void Clear ()
+		{
+			_registeredObjects = new List<RegisteredObject> ();
+		}
 
-        /// <summary>
-        /// Register class.
-        /// </summary>
-        /// <typeparam name="T">Type of class.</typeparam>
-        public static void Register<T>()
+		/// <summary>
+		/// Register class.
+		/// </summary>
+		/// <typeparam name="T">Type of class.</typeparam>
+		public static void Register<T> ()
             where T : new()
-        {
-            Register<T, T>();
-        }
+		{
+			Register<T, T> ();
+		}
 
-        /// <summary>
-        /// Register object.
-        /// </summary>
-        /// <typeparam name="TFrom">That will be requested.</typeparam>
-        /// <typeparam name="TTo">That will actually be returned.</typeparam>
-        public static void Register<TFrom, TTo>()
+		/// <summary>
+		/// Register object.
+		/// </summary>
+		/// <typeparam name="TFrom">That will be requested.</typeparam>
+		/// <typeparam name="TTo">That will actually be returned.</typeparam>
+		public static void Register<TFrom, TTo> ()
             where TTo : TFrom, new()
-        {
-            var registeredObject = new RegisteredObject(() => new TTo(), typeof (TFrom));
-            _registeredObjects.Add(registeredObject);
-        }
+		{
+			var registeredObject = new RegisteredObject (() => new TTo (), typeof(TFrom));
+			_registeredObjects.Add (registeredObject);
+		}
 
-        /// <summary>
-        /// Resolve an instance and dependencies.
-        /// </summary>
-        /// <typeparam name="T">Type of object.</typeparam>
-        /// <exception cref="DependencyMissingException">Throw exception if unable to resove a dependency.</exception>
-        /// <returns>Result object.</returns>
-        public static T Resolve<T>()
-        {
-            T instance;
-            object resolvedObject;
-            bool isResolved = ResolveObject(typeof(T), out resolvedObject);
-            if (isResolved)
-            {
-                instance = (T)resolvedObject;
-            }
-            else
-            {
-                instance = Activator.CreateInstance<T>();
-            }
-            return DoBuildUp(instance);
-        }
+		private static object Register (Type from, Type to)
+		{
+			object instance = to.GetConstructor (new Type[] { }).Invoke (new object[] { });
+			var registeredObject = new RegisteredObject (() => instance, from);
+			_registeredObjects.Add (registeredObject);
 
-        private static T DoBuildUp<T>(T instance)
-        {
-            PropertyInfo[] properties = typeof (T).GetProperties();
-            foreach (PropertyInfo property in properties)
-            {
-                if (!IsResolveProperty(property))
-                {
-                    continue;
-                }
-                object resolvedObject;
-                bool isResolved = ResolveObject(property.PropertyType, out resolvedObject);
-                if (!isResolved)
-                {
-                    throw new DependencyMissingException(string.Format(
-                        "Could not resolve dependency for {0}", typeof (T).Name));
-                }
-                property.SetValue(instance, resolvedObject, null);
-            }
-            return instance;
-        }
+			return instance;
+		}
 
-        private static bool IsResolveProperty(PropertyInfo property)
-        {
-            object[] attributes = property.GetCustomAttributes(typeof (DependencyAttribute), false);
-            return attributes.Length != 0;
-        }
+		/// <summary>
+		/// Resolve an instance and dependencies.
+		/// </summary>
+		/// <typeparam name="T">Type of object.</typeparam>
+		/// <exception cref="DependencyMissingException">Throw exception if unable to resove a dependency.</exception>
+		/// <returns>Result object.</returns>
+		public static T Resolve<T> ()
+		{
+			T instance;
+			object resolvedObject;
+			if (ResolveObject (typeof(T), out resolvedObject) || ResolveAttribute (typeof(T), out resolvedObject))
+ {//			if(ResolveObject(typeof(T), out resolvedObject))
+				instance = (T)resolvedObject;
+			} else {
+				instance = Activator.CreateInstance<T> ();
+			}
+			return DoBuildUp (instance);
+		}
 
-        private static bool ResolveObject(Type typeToResolve, out object resolvedObject)
-        {
-            RegisteredObject result = _registeredObjects.FirstOrDefault(x => x.ObjectType == typeToResolve);
-            if (result == null)
-            {
-                resolvedObject = null;
-                return false;
-            }
-            resolvedObject = result.Instance;
-            return true;
-        }
+		private static T DoBuildUp<T> (T instance)
+		{
+			PropertyInfo[] properties = typeof(T).GetProperties ();
+			foreach (PropertyInfo property in properties) {
+				if (!IsResolveProperty (property)) {
+					continue;
+				}
+				object resolvedObject;
+				bool isResolved = ResolveObject (property.PropertyType, out resolvedObject);
+				if (!isResolved) {
+					throw new DependencyMissingException (string.Format (
+						"Could not resolve dependency for {0}", typeof(T).Name));
+				}
+				property.SetValue (instance, resolvedObject, null);
+			}
+			return instance;
+		}
 
-        private sealed class RegisteredObject
-        {
-            private readonly Lazy<object> _concreteValue;
+		private static bool IsResolveProperty (PropertyInfo property)
+		{
+			object[] attributes = property.GetCustomAttributes (typeof(DependencyAttribute), false);
+			return attributes.Length != 0;
+		}
 
-            public RegisteredObject(Func<object> func, Type objectType)
-            {
-                ObjectType = objectType;
-                _concreteValue = new Lazy<object>(func);
-            }
+		private static bool ResolveObject (Type typeToResolve, out object resolvedObject)
+		{
+			RegisteredObject result = _registeredObjects.FirstOrDefault (x => x.ObjectType == typeToResolve);
+			if (result == null) {
+				resolvedObject = null;
+				return false;
+			}
+			resolvedObject = result.Instance;
+			return true;
+		}
 
-            public object Instance
-            {
-                get { return _concreteValue.Value; }
-            }
+		private static bool ResolveAttribute (Type typeToResolve, out object resolvedObject)
+		{
+			Type resolvedType = null;
+			foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies()) {
+				try {
+					foreach (Type type in assembly.GetTypes()) {
+						var resolvesAttributes = type.GetCustomAttributes (typeof(ResolvesAttribute), false).Cast<ResolvesAttribute> ();
+						if (resolvesAttributes.Any (a => typeToResolve.IsAssignableFrom (a.ResolvesTo))) {
+							resolvedType = type;
+							break;
+						}
+					}
+				} catch (ReflectionTypeLoadException) {
+				}
+				if (resolvedType != null)
+					break;
+			}
+			if (resolvedType == null) {
+				resolvedObject = null;
+				return false;
+			}
 
-            public Type ObjectType { get; private set; }
-        }
-    }
+			resolvedObject = Register (typeToResolve, resolvedType);
+			return true;
+		}
 
-    [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
-    public sealed class DependencyAttribute : Attribute
-    {
-    }
+		private sealed class RegisteredObject
+		{
+			private readonly Lazy<object> _concreteValue;
 
-    [Serializable]
-    public sealed class DependencyMissingException : Exception
-    {
-        public DependencyMissingException()
-        {
-        }
+			public RegisteredObject (Func<object> func, Type objectType)
+			{
+				ObjectType = objectType;
+				_concreteValue = new Lazy<object> (func);
+			}
 
-        public DependencyMissingException(string message) : base(message)
-        {
-        }
+			public object Instance {
+				get { return _concreteValue.Value; }
+			}
 
-        public DependencyMissingException(string message, Exception inner) : base(message, inner)
-        {
-        }
-    }
+			public Type ObjectType { get; private set; }
+		}
+	}
+
+	[AttributeUsage (AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+	public sealed class ResolvesAttribute : Attribute
+	{
+		public Type ResolvesTo;
+
+		public ResolvesAttribute (Type to)
+		{
+			ResolvesTo = to;
+		}
+	}
+
+	[AttributeUsage (AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+	public sealed class DependencyAttribute : Attribute
+	{
+	}
+
+	[Serializable]
+	public sealed class DependencyMissingException : Exception
+	{
+		public DependencyMissingException ()
+		{
+		}
+
+		public DependencyMissingException (string message) : base(message)
+		{
+		}
+
+		public DependencyMissingException (string message, Exception inner) : base(message, inner)
+		{
+		}
+	}
 }
--- UnitTests/TikoContainerTest.cs
+++ UnitTests/TikoContainerTest.cs
@@ -5,76 +5,100 @@ using Tiko;
 namespace UnitTests
 {
 	[TestFixture]
-    public sealed class TikoContainerTest
-    {
-        [Test]
-        public void BuildUp()
-        {
-            TikoContainer.Register<TestProperty>();
-            var result = new TestClass();
-            TikoContainer.BuildUp(result);
-            Assert.IsNull(result.TestProperty1);
-            Assert.IsNotNull(result.TestProperty);
-        }
+	public sealed class TikoContainerTest
+	{
+		[Test]
+		public void BuildUp ()
+		{
+			TikoContainer.Register<TestProperty> ();
+			var result = new TestClass ();
+			TikoContainer.BuildUp (result);
+			Assert.IsNull (result.TestProperty1);
+			Assert.IsNotNull (result.TestProperty);
+		}
 
-        [Test]
-        public void Register()
-        {
-            TikoContainer.Register<ITestClass, TestClass>();
-            var result = TikoContainer.Resolve<ITestClass>();
-            Assert.IsNotNull(result);
-        }
+		[Test]
+		public void Register ()
+		{
+			TikoContainer.Register<ITestClass, TestClass> ();
+			var result = TikoContainer.Resolve<ITestClass> ();
+			Assert.IsNotNull (result);
+		}
 
-        [Test]
-        public void RegisterAsSingleton()
-        {
-            TikoContainer.Register<ITestClass, TestClass>();
-            var result = TikoContainer.Resolve<ITestClass>();
-            var result1 = TikoContainer.Resolve<ITestClass>();
-            Assert.IsNotNull(result1);
-            Assert.IsNotNull(result);
-            Assert.AreEqual(result, result1);
-        }
+		[Test]
+		public void RegisterAsSingleton ()
+		{
+			TikoContainer.Register<ITestClass, TestClass> ();
+			var result = TikoContainer.Resolve<ITestClass> ();
+			var result1 = TikoContainer.Resolve<ITestClass> ();
+			Assert.IsNotNull (result);
+			Assert.IsNotNull (result1);
+			Assert.AreEqual (result, result1);
+		}
 
-        [Test]
-        public void Resolve()
-        {
-            TikoContainer.Register<TestProperty>();
-            var result = TikoContainer.Resolve<TestClass>();
-            Assert.IsNotNull(result);
-            Assert.IsNull(result.TestProperty1);
-            Assert.IsNotNull(result.TestProperty);
-            var result1 = TikoContainer.Resolve<TestClass>();
-            Assert.AreEqual(result.TestProperty1, result1.TestProperty1);
-        }
+		[Test]
+		public void Resolve ()
+		{
+			TikoContainer.Register<TestProperty> ();
+			var result = TikoContainer.Resolve<TestClass> ();
+			Assert.IsNotNull (result);
+			Assert.IsNull (result.TestProperty1);
+			Assert.IsNotNull (result.TestProperty);
+			var result1 = TikoContainer.Resolve<TestClass> ();
+			Assert.AreEqual (result.TestProperty1, result1.TestProperty1);
+		}
 
-        [Test, ExpectedException(typeof (DependencyMissingException))]
-        public void Resolve_MissingDependency()
-        {
-            TikoContainer.Resolve<TestClass>();
-        }
+		[Test, ExpectedException (typeof(DependencyMissingException))]
+		public void Resolve_MissingDependency ()
+		{
+			TikoContainer.Resolve<TestClass> ();
+		}
 
-        [SetUp]
-        public void SetUp()
-        {
-            TikoContainer.Clear();
-        }
-    }
+		[Test]
+		public void ResolveAttribute ()
+		{
+			var result = TikoContainer.Resolve<ITestAttributeClass> ();
+			var result1 = TikoContainer.Resolve<ITestAttributeClass> ();
+			Assert.IsNotNull (result);
+			Assert.IsNotNull (result1);
+			Assert.AreEqual (result, result1);
+		}
 
-    public interface ITestClass
-    {
-    }
+		[SetUp]
+		public void SetUp ()
+		{
+			TikoContainer.Clear ();
+		}
+	}
 
-    public sealed class TestClass : ITestClass
-    {
-        [Dependency]
-        public TestProperty TestProperty { get; set; }
+	public interface ITestClass
+	{
+	}
 
-        public TestProperty TestProperty1 { get; set; }
-    }
+	public sealed class TestClass : ITestClass
+	{
+		[Dependency]
+		public TestProperty TestProperty { get; set; }
 
-    public sealed class TestProperty
-    {
-    }
+		public TestProperty TestProperty1 { get; set; }
+	}
+
+	public sealed class TestProperty
+	{
+	}
+
+	public interface ITestAttributeClass
+	{
+		string Test ();
+	}
+
+	[Resolves (typeof(TestAttributeClass))]
+	public sealed class TestAttributeClass : ITestAttributeClass
+	{
+		public string Test ()
+		{
+			return "Test attribute class";
+		}
+	}
 }
 
